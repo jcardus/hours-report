7c93ad9c8e041e1c4978f719c4dde32b0a5fc35f
jcardeira <joaquim.cardeira@gmail.com>
2026-03-02 20:07:50 -0300

    asssets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   build: {
-    assets: 'reports/hours/_astro',
+    assets: 'reports/hours',
   },
   vite: {
     server: {
